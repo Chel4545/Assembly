951cdd349fcf623cbe5cea55f4cd69807b5cddd6
vasilev.ta <vasilev.ta@dozen.mephi.ru>
2026-03-15 17:40:48 +0300

    make, sort direction value, memory .data refactor

--- 2lab/lab2.asm
+++ 2lab/lab2.asm
@@ -1,14 +1,21 @@
+%ifndef SORT_DIR
+%define SORT_DIR 0
+%endif
+
 section .data
 matrix_rows:
     db 3 ;строки
+
 matrix_col:
     db 3 ;столбцы
 
 matrix:
-    dw 3, 2, 1
-    dw 6, 5, 4
-    dw 9, 8, 7
+    dw 1, 2, 3
+    dw 4, 5, 6
+    dw 7, 8, 9
 
+sort_dir:
+    db SORT_DIR ; направление сортировки (0 - возрастание, 1 - убавание)
 
 section .bss
     tmp: resw 255*255
@@ -25,7 +32,8 @@ _start:
 
     call sort_sum
 
-
+    call fill_tmp_matr
+    call fill_main_matr
 
     jmp ok
 
@@ -52,35 +60,47 @@ sort_sum:
     mov r10, 1
 
     .for_i:
-
+        ; i < n
         cmp r10, rcx
         jae .done
 
         ; x = sum[i]
-        mov r12d, [r8 + r10*4]
+        mov r12d, dword [r8 + r10*4]
 
         ; x_id = ids[i]
-        mov r13d, [r9 + r10*4]
+        mov r13d, dword [r9 + r10*4]
 
         ; j = i
         mov r11, r10
 
-        .while_j:
-
+        .while_j: ;условия выхода из цикла
+            ; j > 0
             test r11, r11
             jz .insert
 
-            ; A[j-1]
-            mov edx, [r8 + r11*4 - 4]    
-            cmp edx, r12d
-            jle .insert
+            ; A[j-1] > x
+            mov edx, dword [r8 + r11*4 - 4]
+            cmp byte [sort_dir], 0
+            je .increase
+            jne .decrease
 
+            .decrease:
+                cmp edx, r12d
+                jge .insert
+                jmp .shift
+
+            .increase:
+                cmp edx, r12d
+                jle .insert
+                jmp .shift
+            
+            .shift:
             ; A[j] = A[j-1]
-            mov [r8 + r11*4], edx
+            mov dword [r8 + r11*4], edx
 
             ; ids[j] = ids[j-1]
-            mov edx, [r9 + r11*4 - 4]
-            mov [r9 + r11*4], edx
+            mov edx, dword [r9 + r11*4 - 4]
+            mov dword [r9 + r11*4], edx
 
             ;j--
             dec r11
@@ -88,10 +108,10 @@ sort_sum:
 
     .insert:
     ;A[j] = x
-    mov [r8 + r11*4], r12d 
+    mov dword [r8 + r11*4], r12d 
 
     ; ids[j] = x_id
-    mov [r9 + r11*4], r13d 
+    mov dword [r9 + r11*4], r13d 
 
     inc r10
     jmp .for_i
@@ -110,115 +130,123 @@ sort_sum:
     pop rbp
     ret
 
-fill_matr:
+fill_tmp_matr:
     push rbp
     mov  rbp, rsp
 
-    movzx r8,  byte [matrix_rows] 
-    movzx r9,  byte [matrix_col] 
-    lea   r10,      [matrix] 
-    lea   r11,      [r9*2]
-
-    xor r12d, r12d
-
-    .loop_fill:
-        cmp r12b, r9b
-        jge .loop_end
-
-        mov r12, [ids + r12b*2]
-        mov ax, word [matrix + r12b*2]
-        mov word []
+    push r8
+    push r9
+    push r10
+    push r11
+    push r13
+    push r14
+    push r15
+    push rax
+    push rbx
+    push rcx
+    push rdx
 
 
-        inc r12b
-        jmp .loop_fill
+    movzx r8, byte [matrix_rows] 
+    movzx r9, byte [matrix_col]
+    lea r10, [matrix]
+    lea r11, [tmp]
+    lea r13, [ids]
 
-    .loop_end:
+    xor r14b, r14b
+
+    .for_col:
+        cmp r14b, r9b
+        jge .cal_end
+
+        mov eax, dword [r13 + r14*4] ; id
+        mov ebx, eax                 ; id
+
+        xor r15b, r15b
+        .for_row:
+            cmp r15b, r8b
+            jge .row_end
+
+
+            mov rax, r15 ; смещение для matr
+            imul rax, r9 ; j*offcet + it
+            add rax, rbx
+
+            mov cx, word [r10 + rax*2] ; база + 2(размер элементов) * смещение
+
+            mov rdx, r15 ; аналогично с первым но для tmp
+            imul rdx, r9
+            add rdx, r14
+
+            mov word [r11 + rdx*2], cx
+
+            inc r15b
+            jmp .for_row
+
+        .row_end:
+ 
+        inc r14b
+        jmp .for_col
+
+    .cal_end:
+
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
+    pop r15
+    pop r14
+    pop r13
+    pop r11
+    pop r10
+    pop r9
+    pop r8
 
     mov rsp, rbp
     pop rbp
-    ret   
+    ret 
 
-; cl=A, dl=B
-swap_blocks:
+fill_main_matr:
     push rbp
     mov  rbp, rsp
 
-    push rbx
-    push rdi
     push r8
     push r9
     push r10
     push r11
     push r12
+    push rax
 
-    movzx r8,  byte [matrix_rows]   ;строки(movzx для инициализации верхних битов 0)
-    movzx r9,  byte [matrix_col] ;колонки
-    lea   r10,      [matrix] ;база
-    lea   r11,      [r9*2]     ;офсет
+    movzx r8,  byte [matrix_rows] 
+    movzx r9,  byte [matrix_col] 
+    lea   r10,      [matrix] 
+    lea   r11,      [tmp]
 
-    lea rbx, [r10 + rcx*2] ;1 столбец
-    lea rdi, [r10 + rdx*2] ;2 столбец
+    xor r12, r12
+    imul r8, r9
 
-    xor r12d, r12d
+    .loop_fill:
+        cmp r12b, r8b
+        jge .loop_end
 
-    .loop_start_A_to_TMP:
-        cmp r12b, r8b ;условие
-        jge .loop_end_A_to_TMP
-
-        mov  rax, r12
-        imul rax, r11             ; rax = r12*r11
-        mov  ax,  word [rbx + rax]
-        mov  word [tmp + r12*2], ax
+        mov ax, word [r11 + r12*2]
+        mov word [r10 + r12*2], ax 
 
         inc r12b
-        jmp .loop_start_A_to_TMP
+        jmp .loop_fill
+    .loop_end:
 
-    .loop_end_A_to_TMP:
-
-    xor r12d, r12d
-
-    .loop_start_B_to_A:
-        cmp r12b, r8b 
-        jge .loop_end_B_to_A
-
-        mov  rax, r12
-        imul rax, r11
-        mov  ax,  word [rdi + rax]
-        mov  word [rbx + rax], ax
-
-        inc r12b
-        jmp .loop_start_B_to_A
-
-    .loop_end_B_to_A:
-
-    xor r12d, r12d
-
-    .loop_start_TMP_to_B:
-        cmp r12b, r8b 
-        jge .loop_end_TMP_to_B
-
-        mov  ax,  word [tmp + r12*2]
-        mov  rax, r12
-        imul rax, r11
-        mov  word [rdi + rax], ax
-
-        inc r12b
-        jmp .loop_start_TMP_to_B
-
-    .loop_end_TMP_to_B:
-
+    pop rax
     pop r12
     pop r11
     pop r10
     pop r9
     pop r8
-    pop rdi
-    pop rbx
 
     mov rsp, rbp
     pop rbp
-    ret
+    ret   
+
 
 fill_sum:
     push rbp
@@ -234,9 +262,9 @@ fill_sum:
         cmp r9b, r8b ;условие
         jge .loop_end
 
-        mov cl, r9b
+        movzx rcx, r9b
         call col_sum 
-        mov [sum + r9*4], eax
+        mov dword [sum + r9*4], eax
 
         inc r9b
         jmp .loop_start
@@ -264,7 +292,7 @@ fill_ids:
         cmp r9b, r8b ;условие
         jge .loop_end
 
-        mov [ids + r9*4], r9
+        mov byte [ids + r9*4], r9b
 
         inc r9b
         jmp .loop_start
@@ -289,11 +317,13 @@ col_sum: ;cl - id столбец
     push r10
     push r11
     push r12
+    push r15
 
     movzx r8,  byte [matrix_rows]   ;строки
     movzx r9,  byte [matrix_col] ;колонки
     lea   r10,      [matrix] ;база
-    lea   r11,      [r9*2] ;офсет
+    mov   r11, r9            ;офсет
+    shl   r11, 1             ;офсет
 
     lea rbx, [r10 + rcx*2] ;начало
 
@@ -314,6 +344,7 @@ col_sum: ;cl - id столбец
 
     .loop_end:
 
+    pop r15
     pop r12
     pop r11
     pop r10
